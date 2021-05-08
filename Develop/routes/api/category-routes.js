39943d401731c163a52cfa9c12bbffe79f135710
Nate Griggs <nateghsc09@gmail.com>
2021-05-08 13:43:43 -0500

    clean up and comments

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,7 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  // starting with a find all
   Category.findAll(
     {
       include: {
@@ -24,6 +25,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  //finds by id value
   Category.findOne({
     where: {
       id: req.params.id
@@ -42,6 +44,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  //creats a new category
   Category.create({
     category_name: req.body.category_name
   })
@@ -55,6 +58,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  // updates a category
   Category.update(
     {
       category_name: req.body.category_name
@@ -79,6 +83,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  // search for a category by id number and then allows us to delete
   Category.destroy({
     where: {
       id: req.params.id
